e606139011aed4a591dcf08da214a6251ea77fb5
Jan Exner <jan.exner@gmx.net>
2019-09-10 14:53:08 +0200

    fixed test setup
    Fixed the test setup for the Role changes

--- bin/__tests__/getIntegrationAccessToken.test.js
+++ bin/__tests__/getIntegrationAccessToken.test.js
@@ -156,7 +156,7 @@ describe('getIntegrationAccessToken', () => {
       );
 
       expect(mockLogVerboseHeader).toHaveBeenCalledWith(
-        'Authenticating with metascope ent_reactor_extension_developer_sdk'
+        'Authenticating with metascope ent_reactor_sdk'
       );
       expect(mockAuth).toHaveBeenCalledWith(expectedAuthOptions());
       expect(accessToken).toBe('generatedAccessToken');
