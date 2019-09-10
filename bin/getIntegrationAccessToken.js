6d166a4970f5070538e22ee084a9edf6e609f9d5
Jan Exner <jan.exner@gmx.net>
2019-09-10 19:13:14 +0200

    Corrected comment to be more specific

--- bin/getIntegrationAccessToken.js
+++ bin/getIntegrationAccessToken.js
@@ -17,7 +17,7 @@ const auth = require('@adobe/jwt-auth');
 
 const METASCOPES = [
   'ent_reactor_sdk',
-  // The below metascopes are necessary to maintain for integrations created before Sep 9, 2019
+  // The below metascopes are necessary to maintain for integrations created before the ent_reactor_sdk metascope existed.
   'ent_reactor_extension_developer_sdk',
   'ent_reactor_admin_sdk',
 ];
